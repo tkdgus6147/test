246468249771ad39a2c96d09321b0950da5a3c0a
sanghyun1.choi <sanghyun1.choi@hlcompany.com>
2023-10-11 11:52:01 +0900

    add test.c

--- dev/main.c
+++ dev/main.c
@@ -2,21 +2,11 @@
 
 int main(){
     printf("hello world!\n");
-
-<<<<<<< HEAD
-    printf("test111111111"\n);
-=======
-    printf("test222222222\n");
-
-    printf("test33333333\n");
->>>>>>> origin/test
-
-<<<<<<< HEAD
-    printf("test444444\n");
-
 =======
     printf("test112233\n");
 >>>>>>> origin/main
+
+    printf("test445566\n");
 
     return 0;
 }
